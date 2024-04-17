04622d36e89ada75204b69aa006c745ff2850f2e
LiwDev <liw.dev.23@gmail.com>
2024-04-17 00:37:41 -0300

    Iniciando projeto controle delivery do restaurante

---
+++ src/main/java/br/com/menberket/academywakanda/deliveryrestaurante/DeliveryRestauranteApplication.java
@@ -0,0 +1,13 @@
+package br.com.menberket.academywakanda.deliveryrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/menberket/academywakanda/deliveryrestaurante/DeliveryRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.menberket.academywakanda.deliveryrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
